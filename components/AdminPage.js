7d7ca63deca82ca15005dcdb108feeb3aa5c0258
Frode Hansen <frodehansen@Frodes-MacBook-Pro.local>
2019-11-28 11:13:58 +0100

    Enable staging build

--- components/AdminPage.js
+++ components/AdminPage.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import PublishComponent from './PublishComponent';
 
-// const stagingPayload = {
-//     ref: 'staging',
-//     environment: 'dev-sbs',
-//     auto_merge: false,
-//     required_context: []
-// };
+const stagingPayload = {
+    ref: 'staging',
+    environment: 'dev-sbs',
+    auto_merge: false,
+    required_context: []
+};
 
 const productionPayload = {
     ref: 'master',
@@ -19,11 +19,11 @@ export default class AdminPage extends React.Component {
     render() {
         return (
             <div style={{ padding: ' 1rem' }}>
-                {/* <PublishComponent
+                <PublishComponent
                     title="Staging"
                     data={stagingPayload}
                     description="Oppdater staging-miljøet med alt publisert innhold fra Sanity staging datasettet."
-                /> */}
+                />
                 <PublishComponent
                     title="Produksjon"
                     data={productionPayload}
