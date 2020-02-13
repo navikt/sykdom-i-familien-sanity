05111dc653ab57cd2702541ff576d0c97b7721be
Frode Hansen <frode.hansen@bekk.no>
2020-02-13 23:29:39 +0100

    Gruppere sekundære menyvalg

--- config/deskStructure.js
+++ config/deskStructure.js
@@ -111,7 +111,6 @@ export default () =>
                 )
                 .icon(() => <YtelsePageIcon isPublic={false} />),
             S.divider(),
-
             S.listItem()
                 .title('Administrasjon')
                 .child(
@@ -120,32 +119,44 @@ export default () =>
                         .component(AdminPage)
                 )
                 .icon(AdminIcon),
+            S.divider(),
+            S.divider(),
+            S.divider(),
             S.listItem()
-                .title('Ytelser')
-                .child(S.documentTypeList('ytelse')),
-            S.listItem()
-                .title('Illustrasjoner etter kategori')
+                .title('Annet')
                 .child(
-                    S.documentList()
+                    S.list('other')
                         .title('Kategorier')
-                        .menuItems(
-                            S.documentTypeList('illustrationCategory').getMenuItems(),
-                            S.documentTypeList('illustration').getMenuItems()
-                        )
-                        .filter('_type == $type && !defined(parents)')
-                        .params({ type: 'illustrationCategory' })
-                        .child((categoryId) =>
-                            S.documentList()
-                                .title('Illustrasjoner')
-                                .menuItems(S.documentTypeList('illustration').getMenuItems())
-                                .filter('_type == $type && $categoryId == category._ref')
-                                .params({ type: 'illustration', categoryId })
-                        )
-                ),
-            S.listItem()
-                .title('Illustrasjonskategorier')
-                .child(S.documentTypeList('illustrationCategory')),
-            S.listItem()
-                .title('Lenker')
-                .child(S.documentTypeList('link'))
+                        .items([
+                            S.listItem()
+                                .title('Illustrasjonskategorier')
+                                .child(S.documentTypeList('illustrationCategory')),
+
+                            S.listItem()
+                                .title('Ytelser')
+                                .child(S.documentTypeList('ytelse')),
+                            S.listItem()
+                                .title('Illustrasjoner etter kategori')
+                                .child(
+                                    S.documentList()
+                                        .title('Kategorier')
+                                        .menuItems(
+                                            S.documentTypeList('illustrationCategory').getMenuItems(),
+                                            S.documentTypeList('illustration').getMenuItems()
+                                        )
+                                        .filter('_type == $type && !defined(parents)')
+                                        .params({ type: 'illustrationCategory' })
+                                        .child((categoryId) =>
+                                            S.documentList()
+                                                .title('Illustrasjoner')
+                                                .menuItems(S.documentTypeList('illustration').getMenuItems())
+                                                .filter('_type == $type && $categoryId == category._ref')
+                                                .params({ type: 'illustration', categoryId })
+                                        )
+                                ),
+                            S.listItem()
+                                .title('Lenker')
+                                .child(S.documentTypeList('link'))
+                        ])
+                )
         ]);
