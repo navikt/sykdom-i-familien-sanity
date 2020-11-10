9dc31599598b8ff4f6322759dfc7e8e6206edffa
Frode Hansen <frode.hansen@bekk.no>
2020-11-10 09:34:59 +0100

    Update publishComponent layout

--- components/PublishComponent.js
+++ components/PublishComponent.js
@@ -48,57 +48,54 @@ export default class EnvPublish extends React.Component {
         };
 
         return (
-            <Fieldset legend={title}>
-                <p>{description}</p>
-                {url && (
-                    <p>
-                        <a href={url}>{}</a>
-                    </p>
-                )}
-                {!hasTokenFromDocument && (
-                    <div style={{ marginBottom: '1rem' }}>
-                        <FormField label="Token">
-                            <Input
-                                id="token_input"
-                                type="text"
-                                value={token}
-                                onChange={(evt) => this.setState({ token: evt.target.value })}
-                            />
-                        </FormField>
-                    </div>
-                )}
-                <Button
-                    style={{ width: '10rem', height: '2.5rem' }}
-                    onClick={
-                        token && token !== ''
-                            ? () => {
-                                  this.setState({ pending: true, error: undefined, done: false });
-                                  triggerBuild(data, token, onSuccess, onError);
-                              }
-                            : undefined
-                    }
-                    disabled={pending}>
-                    {pending && (
-                        <span>
-                            <LoadingSpinner />
-                        </span>
+            <Fieldset legend={title} style={{ marginBottom: '2rem' }}>
+                <div style={{ paddingBottom: '1rem' }}>
+                    {description && <p>{description}</p>}
+                    {!hasTokenFromDocument && (
+                        <div style={{ marginBottom: '1rem' }}>
+                            <FormField label="Token">
+                                <Input
+                                    id="token_input"
+                                    type="text"
+                                    value={token}
+                                    onChange={(evt) => this.setState({ token: evt.target.value })}
+                                />
+                            </FormField>
+                        </div>
                     )}
-                    {!pending && <span style={{ display: 'flex', flexWrap: 'nowrap' }}>{buttonLabel}</span>}
-                </Button>
+                    <Button
+                        style={{ width: '10rem', height: '2.5rem' }}
+                        onClick={
+                            token && token !== ''
+                                ? () => {
+                                      this.setState({ pending: true, error: undefined, done: false });
+                                      triggerBuild(data, token, onSuccess, onError);
+                                  }
+                                : undefined
+                        }
+                        disabled={pending}>
+                        {pending && (
+                            <span>
+                                <LoadingSpinner />
+                            </span>
+                        )}
+                        {!pending && <span style={{ display: 'flex', flexWrap: 'nowrap' }}>{buttonLabel}</span>}
+                    </Button>
 
-                {pending !== true && (
-                    <>
-                        {error !== undefined && (
-                            <Message>
-                                <p>Det oppstod en feil under oppdateringen}</p>
-                                <code>[{error}]</code>
-                            </Message>
-                        )}
-                        {done === true && error === undefined && (
-                            <Message>Ny versjon er på vei ut. Dette tar 4-5 minutter.</Message>
-                        )}
-                    </>
-                )}
+                    {pending !== true && (
+                        <>
+                            {error !== undefined && (
+                                <Message>
+                                    <p>Det oppstod en feil under oppdateringen</p>
+                                    <code>[{error}]</code>
+                                </Message>
+                            )}
+                            {done === true && error === undefined && (
+                                <Message>Ny versjon er på vei ut. Dette tar 4-5 minutter.</Message>
+                            )}
+                        </>
+                    )}
+                </div>
             </Fieldset>
         );
     }
