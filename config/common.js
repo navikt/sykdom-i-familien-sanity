64fa2fde94754a4afbecf1459f70ecfbd8f476a4
Frode Hansen <frode.hansen@bekk.no>
2020-12-07 13:25:27 +0100

    Cleanup

--- config/common.js
+++ config/common.js
@@ -1,2 +1,1 @@
-export const previewURL = 'http://localhost:8000';
-// export const previewURL = 'https://sykdom-i-familien.dev.nav.no';
+export const previewURL = 'https://sykdom-i-familien.dev.nav.no';
